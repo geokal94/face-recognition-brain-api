542744f2df0b45ce81494afe0ea55809528324f3
George Kallis <giorgos.kallis.gr@gmail.com>
2019-05-22 15:12:07 +0100

    commit

--- server.js
+++ server.js
@@ -64,6 +64,6 @@ app.put('/image', (req,res) => { image.handleImage(req,res,db)});
 app.post('/imageUrl', (req,res) => { image.handleApiCall(req,res,db)});
 
 
-app.listen(process.env.PORT || 3000 , () => {
+app.listen(3000 , () => {
     console.log(`app is running on port ${process.env.PORT}`);
 });
